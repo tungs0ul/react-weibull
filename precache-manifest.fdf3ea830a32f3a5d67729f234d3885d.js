300f7ff2de198ce1dd1716a40b7049292e87f319
Tung <tungvn179@gmail.com>
2021-08-26 17:01:54 +0200

    Updates

---
+++ precache-manifest.fdf3ea830a32f3a5d67729f234d3885d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887ac302d9e28469fc2eab5d8f7d98de",
+    "url": "./index.html"
+  },
+  {
+    "revision": "84211cfeb20bd02bfbc0",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "1f9313f2040798aec125",
+    "url": "./static/js/2.475250ef.chunk.js"
+  },
+  {
+    "revision": "b8f367e878583fdb631c506689d4543c",
+    "url": "./static/js/2.475250ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84211cfeb20bd02bfbc0",
+    "url": "./static/js/main.08eeccac.chunk.js"
+  },
+  {
+    "revision": "2ab9078b027b29fd1787",
+    "url": "./static/js/runtime-main.0179494f.js"
+  }
+]);
